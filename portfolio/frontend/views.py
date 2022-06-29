7037cef80b9240fc4c30e90cc97c1584c0e8acbf
ofori_Prince <Pofori034@st.ug.edu.gh>
2022-06-29 13:12:38 +0000

    render data from database

--- portfolio/frontend/views.py
+++ portfolio/frontend/views.py
@@ -1,8 +1,14 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.views import View
-
+from backend.models import *
+from accounts.models import User
 
 class indexView(View):
    def get(self, request):
       template_name = 'website/index.html'
-      return render(request, template_name)
+      user = User.objects.all().first()
+      context = {
+         'user': user,
+      }
+      return render(request, template_name,context)
