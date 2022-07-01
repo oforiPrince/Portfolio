ca6fc0c5b951183801e109c285c6f166ce451a48
ofori_Prince <Pofori034@st.ug.edu.gh>
2022-07-01 15:15:57 +0000

    setup dashboard

--- portfolio/accounts/views.py
+++ portfolio/accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request):
+        template_name = 'login.html'
+
+        return render(request, template_name)
--- portfolio/backend/urls.py
+++ portfolio/backend/urls.py
@@ -1,3 +1,9 @@
+from django.urls import path
+
+from . import views
+
+
 urlpatterns = [
-    
+    path('',views.DashboardView.as_view(),name='dashboard'),
+    path('profile/',views.ProfileView.as_view(),name='profile'),
 ]
