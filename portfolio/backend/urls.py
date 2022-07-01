b80b31fd29fd916b949e0d787a87603dff606538
ofori_Prince <Pofori034@st.ug.edu.gh>
2022-07-01 16:31:55 +0000

    making dashboard dynamic

--- portfolio/backend/urls.py
+++ portfolio/backend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-
+app_name = 'backend'
 urlpatterns = [
     path('',views.DashboardView.as_view(),name='dashboard'),
     path('profile/',views.ProfileView.as_view(),name='profile'),
